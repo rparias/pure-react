873f40fa5607e553cec3869819336aa747704aa3
Ronald Arias <ariasron@hotmail.com>
2021-07-05 12:46:29 -0500

    Passing props from Parent App to Child Pet

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement('div', {}, [
-        React.createElement('h2', {}, 'Lory'),
-        React.createElement('h3', {}, 'Dog'),
-        React.createElement('h3', {}, 'Cocker'),
+        React.createElement('h2', {}, props.name),
+        React.createElement('h3', {}, props.animal),
+        React.createElement('h3', {}, props.breed),
     ]);
 }
 
 const App = () => {
     return React.createElement('div', {}, [
         React.createElement('h1', {id: 'my-logo'}, 'Adpot me!'),
-        React.createElement(Pet),
-        React.createElement(Pet),
-        React.createElement(Pet)
+        React.createElement(Pet, {name: 'Lory', animal: 'Dog', breed: 'Cocker'}),
+        React.createElement(Pet, {name: 'Zeus', animal: 'Dog', breed: 'Unknown'}),
+        React.createElement(Pet, {name: 'Quarzis', animal: 'Dog', breed: 'Unknown'}),
     ]);
 }
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
