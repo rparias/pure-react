a12feea8ce213852510d7ff47b0b34ad1b28f0e1
Ronald Arias <ariasron@hotmail.com>
2021-07-05 21:48:24 -0500

    Adds a dropdown component for breeds

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit"];
+const BREEDS = [];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Quito, EC");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
 
   function updateLocation(e) {
     setLocation(e.target.value);
@@ -17,7 +22,38 @@ const SearchParams = () => {
             type="text"
             placeholder="Location"
             onChange={updateLocation}
+            onBlur={updateLocation}
           />
+        </label>
+        <label htmlFor="animal">
+          <select
+            name="animal"
+            id="animal"
+            onChange={(e) => setAnimal(e.target.value)}
+          >
+            {ANIMALS.map((animal, key) => {
+              return (
+                <option value={animal} key={key}>
+                  {animal}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          <select
+            name="breed"
+            id="breed"
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            {BREEDS.map((breed, key) => {
+              return (
+                <option value={breed} key={key}>
+                  {breed}
+                </option>
+              );
+            })}
+          </select>
         </label>
         <button>Submit</button>
       </form>
