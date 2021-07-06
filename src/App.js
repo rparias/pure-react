3a3d7b92cbfa6d803a6b233274e4a27b4c8c8758
Ronald Arias <ariasron@hotmail.com>
2021-07-05 20:25:34 -0500

    Refactors to use jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", { id: "my-logo" }, "Adpot me!"),
+//     React.createElement(Pet, { name: "Lory", animal: "Dog", breed: "Cocker" }),
+//     React.createElement(Pet, { name: "Zeus", animal: "Dog", breed: "Unknown" }),
+//     React.createElement(Pet, {
+//       name: "Quarzis",
+//       animal: "Dog",
+//       breed: "Unknown",
+//     }),
+//   ]);
+// };
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-logo" }, "Adpot me!"),
-    React.createElement(Pet, { name: "Lory", animal: "Dog", breed: "Cocker" }),
-    React.createElement(Pet, { name: "Zeus", animal: "Dog", breed: "Unknown" }),
-    React.createElement(Pet, {
-      name: "Quarzis",
-      animal: "Dog",
-      breed: "Unknown",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adpot me!</h1>
+      <Pet name="Lory" animal="Dog" breed="Cocker" />
+      <Pet name="Zeus" animal="Dog" breed="Unknown" />
+      <Pet name="Quarzis" animal="Dog" breed="Unknown" />
+    </div>
+  );
 };
+
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
