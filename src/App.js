9e033425bd634083787afb945567640d09507c2f
Ronald Arias <ariasron@hotmail.com>
2021-07-05 20:37:56 -0500

    Updates eslint for react

--- src/App.js
+++ src/App.js
@@ -2,19 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", { id: "my-logo" }, "Adpot me!"),
-//     React.createElement(Pet, { name: "Lory", animal: "Dog", breed: "Cocker" }),
-//     React.createElement(Pet, { name: "Zeus", animal: "Dog", breed: "Unknown" }),
-//     React.createElement(Pet, {
-//       name: "Quarzis",
-//       animal: "Dog",
-//       breed: "Unknown",
-//     }),
-//   ]);
-// };
-
 const App = () => {
   return (
     <div>
@@ -26,4 +13,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
