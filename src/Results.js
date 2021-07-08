640413cbae6982a5dfe4d9b15aafad70573f05fa
Ronald Arias <ariasron@hotmail.com>
2021-07-07 21:19:00 -0500

    Refactors code creating component composition

--- src/Results.js
+++ src/Results.js
@@ -2,15 +2,24 @@ import Pet from "./Pet";
 
 const Results = ({ pets }) => {
   return (
-    <div>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => {
+          return (
+            <Pet
+              animal={pet.animal}
+              key={pet.id}
+              name={pet.name}
+              breed={pet.breed}
+              images={pet.images}
+              location={`${pet.city}, ${pet.state}`}
+              id={pet.id}
+            />
+          );
+        })
+      )}
     </div>
   );
 };
