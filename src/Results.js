a54d25c627079fcdf9362d1cb4585dfbb7f1ac61
Ronald Arias <ariasron@hotmail.com>
2021-07-07 21:12:10 -0500

    Extracts results in a new file

---
+++ src/Results.js
@@ -0,0 +1,18 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Results;
