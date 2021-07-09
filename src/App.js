c1259b59fd4a0960d1b6a47e493665a5989151cd
Ronald Arias <ariasron@hotmail.com>
2021-07-08 20:20:16 -0500

    Uses Link Tag to prevent the reload of the whole page, it works like a SPA

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 import { StrictMode } from "react";
@@ -8,8 +8,12 @@ import { StrictMode } from "react";
 const App = () => {
   return (
     <div>
-      <h1>Adpot me!</h1>
       <Router>
+        <header>
+          <Link to="/">
+            <h1>Adpot me!</h1>
+          </Link>
+        </header>
         <Switch>
           <Route path="/details/:id">
             <Details />
