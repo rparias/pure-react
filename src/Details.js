2ee06f3707ee8bdc4bd8c18a36230f9147d64f79
Ronald Arias <ariasron@hotmail.com>
2021-07-08 20:43:46 -0500

    Adds elements on details page, and withRouter on Details Class to pass the parameters on url

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,46 @@
-const Details = () => {
-  return <h2>Hi, this is detail page</h2>;
-};
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-export default Details;
+class Details extends Component {
+  constructor() {
+    super();
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const id = this.props.match.params.id;
+    const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+
+  render() {
+    console.log(this.state);
+
+    if (this.state.loading) {
+      return <h2>loading … </h2>;
+    }
+
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
