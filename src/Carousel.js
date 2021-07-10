bc2e256b2f9b67d460857b3209864f58c8626b43
Ronald Arias <ariasron@hotmail.com>
2021-07-10 16:00:13 -0500

    Displays active image when clicked on carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -2,27 +2,36 @@ import { Component } from "react";
 
 class Carousel extends Component {
   state = {
-    active: 0,
+    indexActiveImage: 0,
   };
 
   static defaultProps = {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  handleIndexClick = (event) => {
+    this.setState({
+      indexActiveImage: +event.target.dataset.index, //0 + index
+    });
+  };
+
   render() {
-    const { active } = this.state;
+    const { indexActiveImage } = this.state;
     const { images } = this.props;
 
     return (
       <div className="carousel">
-        <img src={images[active]} alt="animal" />
+        <img src={images[indexActiveImage]} alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line
             <img
               key={photo}
               src={photo}
+              onClick={this.handleIndexClick}
+              data-index={index}
               alt="animal"
-              className={index === active ? "active" : ""}
+              className={index === indexActiveImage ? "active" : ""}
             />
           ))}
         </div>
